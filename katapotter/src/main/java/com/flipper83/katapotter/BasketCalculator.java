3898a552b98be44634a4228622c5cda4d89c6a2f
Jorge J Barroso <jorge@karumi.com>
2016-01-24 20:00:42 +0100

    [RED] test empty basket

---
+++ katapotter/src/main/java/com/flipper83/katapotter/BasketCalculator.java
@@ -0,0 +1,9 @@
+package com.flipper83.katapotter;
+
+import java.util.List;
+
+public class BasketCalculator {
+    public int calculatePrice(List<String> books) {
+        return -1;
+    }
+}
--- katapotter/src/test/java/com/flipper83/katapotter/BasketCalculatorTest.java
+++ katapotter/src/test/java/com/flipper83/katapotter/BasketCalculatorTest.java
@@ -1,4 +1,20 @@
 package com.flipper83.katapotter;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BasketCalculatorTest {
+  private static final List<String> EMPTY_BASKET = new ArrayList<>();
+
+  @Test
+  public void shouldReturnZeroPriceForAnEmptyBasket() {
+    BasketCalculator basketCalculator = new BasketCalculator();
+
+    int amount = basketCalculator.calculatePrice(EMPTY_BASKET);
+
+    assertEquals(0, amount);
+  }
 }
